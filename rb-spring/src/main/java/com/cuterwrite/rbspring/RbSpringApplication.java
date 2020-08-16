f9372350159d274335be80f4b74dd118a9213791
Pang Shangzhi <158505862@qq.com>
2020-08-16 16:46:32 +0800

    创建spring boot项目

---
+++ rb-spring/src/main/java/com/cuterwrite/rbspring/RbSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cuterwrite.rbspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbSpringApplication.class, args);
+	}
+
+}
---
+++ rb-spring/src/test/java/com/cuterwrite/rbspring/RbSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuterwrite.rbspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
